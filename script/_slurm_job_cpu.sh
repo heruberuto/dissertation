a5c66a1a601247d713f881d7eb634bb323336783
Herbert Ullrich <ullriher@fel.cvut.cz>
2025-08-20 16:30:05 +0200

    technical issues resolved

---
+++ script/_slurm_job_cpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition amd
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem-per-cpu 96G
+#SBATCH --time 24:00:00
+#SBATCH --job-name averitec
+#SBATCH --output logs/jupyter.%j.out
+
+# Replace with your own virtual environment
+ml Python/3.12.3-GCCcore-13.3.0
+#ml CUDA/12.6.0
+ml OpenSSL/3
+
+source /mnt/personal/ullriher/venvs/aug25/bin/activate
+
+# Replace with absolute path to your project or sbatch from project root and comment out
+# cd ~/ullriher/aic_averitec
+# load your .env
+export $(grep -v '^#' .env | xargs)
+
+export PYTHONPATH=src:$PYTHONPATH
+jupyter notebook --no-browser --port=$(shuf -i8000-9999 -n1) --ip=$(hostname -s)
+
